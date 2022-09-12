f4dae3acccab388ab2498f29da770fa853028744
Sylvain Hellegouarch <sh@defuze.org>
2022-09-12 22:27:48 +0200

    add dirs
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
+import asyncio
+import os
 import secrets
 
-from ghapi.all import GhApi
+import httpx
 
 
 ACTION = """name: Run Reliably Experiment
@@ -11,8 +13,9 @@ on:
     - main
 
 jobs:
-  build:
+  run-experiment:
     runs-on: ubuntu-latest
+    environment: myenv
     steps:
     - uses: actions/checkout@v3
     - name: Set up Python
@@ -28,36 +31,81 @@ jobs:
         chaos info core
 """
 
-def make_blob(api: GhApi) -> str:
-    return api.git.create_blob(content=ACTION, encoding="utf-8")["sha"]
-
-def make_tree(api: GhApi, blob_sha: str) -> str:
-    last_commit_sha = api.git.get_ref("heads/main")["object"]["sha"]
-    name = secrets.token_hex(8)
-    return api.git.create_tree(
-        base_treestring=last_commit_sha,
-        tree=[{
-            "path": f".github/workflows/action-{name}.yaml",
-            "mode": "100644",
-            "type": "blob",
-            "sha": blob_sha
-        }]
-    )["sha"]
+async def make_blob(client: httpx.AsyncClient, owner: str, repo: str,) -> str:
+    response = await client.post(
+        f"/repos/{owner}/{repo}/git/blobs",
+        json={
+            "content": ACTION,
+            "encoding": "utf-8"
+        }
+    )
+    return response.json()["sha"]
 
 
-def make_commit(api: GhApi, tree_sha: str) -> str:
-    return api.git.create_tree(
-        message="hello",
-        tree=tree_sha
-    )["sha"]
+async def make_tree(client: httpx.AsyncClient, owner: str, repo: str, last_commit_sha: str) -> str:
+    blob_sha = await make_blob(client, owner, repo)
+    response = await client.post(
+        f"/repos/{owner}/{repo}/git/trees",
+        json={
+            "base_tree": last_commit_sha,
+            "tree": [
+                {
+                    "path": f".github/workflows/action-{secrets.token_hex(8)}.yaml",
+                    "mode": "100644",
+                    "type": "blob",
+                    "sha": blob_sha
+                }
+            ]
+        }
+    )
+    assert response.status_code == 201, response.json()
+    return response.json()["sha"]
 
 
-def run():
-    api = GhApi(owner="Lawouach", repo="ctkgh")
-    sha = make_blob(api)
-    sha = make_tree(api, sha)
-    make_commit(api, sha)
+
+async def get_last_sha(client: httpx.AsyncClient, owner: str, repo: str) -> str:
+    response = await client.get(
+        f"/repos/{owner}/{repo}/git/refs/heads/main"
+    )
+    return response.json()["object"]["sha"]
+
+
+async def make_commit(client: httpx.AsyncClient, owner: str, repo: str) -> str:
+    last_commit_sha = await get_last_sha(client, owner, repo)
+    tree_sha = await make_tree(client, owner, repo, last_commit_sha)
+
+    response = await client.post(
+        f"/repos/{owner}/{repo}/git/commits",
+        json={
+            "message": "hello",
+            "parents": [last_commit_sha],
+            "tree": tree_sha
+        }
+    )
+    return response.json()["sha"]
+
+
+async def update_ref(client: httpx.AsyncClient, owner: str, repo: str) -> str:
+    sha = await make_commit(client, owner, repo)
+    response = await client.post(
+        f"/repos/{owner}/{repo}/git/refs/heads/main",
+        json={
+            "ref": "refs/heads/main",
+            "sha": sha
+        }
+    )
+    print(response.json())
+
+
+async def run(owner: str, repo: str, token: str) -> str:
+    async with httpx.AsyncClient() as client:
+        client.base_url = "https://api.github.com"
+        client.headers = {
+            "Accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {token}"
+        }
+        await update_ref(client, owner, repo)
 
 
 if __name__ == "__main__":
-    run()
+    asyncio.run(run("Lawouach", "ctkgh", os.getenv("GITHUB_TOKEN")))
